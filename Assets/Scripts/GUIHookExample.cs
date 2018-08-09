75799738938a076b0612adb8c183de7c38895817
duneta <adunetz1@gmail.com>
2018-08-09 19:40:15 -0400

    add example scene GUIExample.unity to show how buttons and other ui features can be done.

---
+++ Assets/Scripts/GUIHookExample.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GUIHookExample : MonoBehaviour {
+
+	public Text text;
+
+	public string textA;
+	public string textB;
+
+	public bool isTextA = true;
+
+	// Use this for initialization
+	void Start () {
+		if (text == null)
+		{ throw new System.Exception("GUIHookExample object "+gameObject.name+" does not have a GUIText object associated."); }
+
+		text.text = textA;
+	}
+	
+	public void ToggleText()
+	{
+		if (isTextA)
+		{ text.text = textB; }
+		else 
+		{ text.text = textA; }
+		isTextA = !isTextA;
+	}
+}
