2c84989617870d052a8aed92769d1f9be9d92f43
duneta <adunetz1@gmail.com>
2018-08-09 18:56:13 -0400

    initial commit creating project

---
+++ Assets/Scripts/SampleScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SampleScript : MonoBehaviour {
+
+	/** Note from Andrew
+		This script current does nothing.
+	 */
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
