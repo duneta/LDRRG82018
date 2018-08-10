cf772ed0925d135e29c97832af04d2fc869ae618
duneta <adunetz1@gmail.com>
2018-08-10 18:01:52 -0400

    add another example based on top-down movement

---
+++ Assets/Scripts/TopDownMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownMovement : MonoBehaviour {
+
+	public float speed = 5;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		/// get raw input values
+		float h = Input.GetAxis("Horizontal");
+		float v = Input.GetAxis("Vertical");
+
+		Vector3 movement  = new Vector3(h, v, 0);
+		
+		/// prevents diagonals from being faster.
+		if (movement.magnitude > 1)
+		{ movement.Normalize(); }
+
+		/// scale movement to duration of frame
+		movement *= (speed * Time.deltaTime);
+
+		/// update the position
+		transform.position = transform.position + movement;
+
+	}
+}
