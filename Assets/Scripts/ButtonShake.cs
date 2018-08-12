a94030ad7979e9c56218ed411ab99180b66e5057
duneta <adunetz1@gmail.com>
2018-08-11 21:19:58 -0400

    make more phone lock stuff

---
+++ Assets/Scripts/ButtonShake.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ButtonShake : MonoBehaviour {
+
+	Vector3 canonPosition;
+	float timer = 0;
+	float shakeTime = 0.5f;
+	bool shaking = false;
+
+	public void Shake()
+	{
+		shaking = true;
+		timer = Time.time;
+	}
+
+	// Use this for initialization
+	void Start () {
+		canonPosition = transform.position;
+		Button b = GetComponent<Button>();
+		b.onClick.AddListener(Shake);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (shaking)
+		{
+			if (Time.time - timer > shakeTime)
+			{
+				shaking = false;
+				transform.position = canonPosition;
+			}
+			else
+			{
+				float normalTime = (Time.time - timer) / shakeTime;
+				Vector3 shakeDisplacement = new Vector3((1.0f-normalTime)*(normalTime*Mathf.Sin(10*normalTime)*20),0,0);
+				transform.position = canonPosition + shakeDisplacement;
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/RequireText.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class RequireText : MonoBehaviour {
+
+	public UnityEvent onfinalText;
+
+	Button button;
+
+	public InputField field;
+
+	public string output;
+	
+	// Use this for initialization
+	void Start () {
+		button = GetComponent<Button>();
+		button.interactable = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnTextFinal()
+	{
+		if (onfinalText != null)
+		{ onfinalText.Invoke(); }
+	}
+
+	public void OnTextEdit(string inbound)
+	{
+		output = inbound;
+		button.interactable = inbound != string.Empty;
+	}
+}
