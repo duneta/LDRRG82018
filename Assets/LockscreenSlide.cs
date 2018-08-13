a7be6fcc33f7ec35cdbe54db0a9ac722dcc82b36
Marc <likedudeomfglmao1@gmail.com>
2018-08-12 20:22:55 -0500

    Fix lockscreen issue with animation event

---
+++ Assets/LockscreenSlide.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class LockscreenSlide : MonoBehaviour {
+public Animator LockscreenSlideAnimation; 
+public Scrollbar scrollbar;
+public SlideBack slideback;
+public LockscreenSlide DeactivateLockscreen;
+
+public
+	// Use this for initialization
+	void Start () {
+		LockscreenSlideAnimation = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void SlideAway() {
+		LockscreenSlideAnimation.SetTrigger("SlideToRight");
+	}
+
+	public void TurnOff() {
+		transform.parent.gameObject.SetActive(false);
+	}
+}
--- Assets/SlideBack.cs
+++ Assets/SlideBack.cs
@@ -5,10 +5,12 @@ using UnityEngine.UI;
 using UnityEngine.Events;
 
 public class SlideBack : MonoBehaviour {
+
 	float velocity = 0;
 	float acceleration = -0.1f;
 	Scrollbar scrollbar;
 	public UnityEvent OnSlideRight;
+	public bool firsttime = true;
 	// Use this for initialization
 	void Start () {
 		scrollbar = GetComponent<Scrollbar>();
@@ -18,18 +20,21 @@ public class SlideBack : MonoBehaviour {
 	}
 	// Update is called once per frame
 	void Update () {
-		if (!Input.GetMouseButton(0)) {velocity += acceleration;
+		if (!Input.GetMouseButton(0) && scrollbar.value < 0.999) {velocity += acceleration;
 			scrollbar.value = Mathf.Max(0,scrollbar.value+velocity);
 		}
 
 		if (scrollbar.value > 0.999) {
+			
 			scrollbar.interactable = false;
 
-			if (OnSlideRight != null) {
-				OnSlideRight.Invoke();
+			if (OnSlideRight != null){
+				
+				if (firsttime) {
+					OnSlideRight.Invoke();
+					firsttime=false;
+				}	
 			}
-		}
-
-		
+		}	
 	}
 }
