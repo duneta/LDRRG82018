afefd1962a1633d6bb6eda4227164ae86efa74ad
duneta <adunetz1@gmail.com>
2018-08-13 03:22:26 -0400

    integrate the music

--- Assets/Scripts/ButtonShake.cs
+++ Assets/Scripts/ButtonShake.cs
@@ -8,7 +8,7 @@ public class ButtonShake : MonoBehaviour {
 
 	Vector3 canonPosition;
 	float timer = 0;
-	float shakeTime = 0.5f;
+	float shakeTime = 0.75f;
 	bool shaking = false;
 
 	public void Shake()
@@ -36,7 +36,7 @@ public class ButtonShake : MonoBehaviour {
 			else
 			{
 				float normalTime = (Time.time - timer) / shakeTime;
-				Vector3 shakeDisplacement = new Vector3((1.0f-normalTime)*(normalTime*Mathf.Sin(10*normalTime)*20),0,0);
+				Vector3 shakeDisplacement = new Vector3((1.0f-normalTime)*(normalTime*Mathf.Sin(15*normalTime)*40),0,0);
 				transform.position = canonPosition + shakeDisplacement;
 			}
 		}
---
+++ Assets/SoundOwner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundOwner : MonoBehaviour {
+
+	AudioSource source;
+
+	// Use this for initialization
+	void Start () {
+		source = GetComponent<AudioSource>();
+		source.UnPause();
+	}
+
+	void OnDisable()
+	{
+		source.Pause();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
